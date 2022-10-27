c22450b1c7916f31f092d05e8a8a33d174e90756
Ilchan <annilchen@yandex.ru>
2022-10-27 13:56:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите ваш Вес");
+double v = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите ваш Рост (см)");
+double r = Convert.ToDouble(Console.ReadLine());
+r = r / 100;
+double imt = Math.Round(v / Math.Pow(r, 2),2) ;
+Console.WriteLine($"Имт равен {imt}");
